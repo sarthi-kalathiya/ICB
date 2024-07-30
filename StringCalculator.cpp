79ff040c2fe0b11d26da26a6bda3a9e5024ea290
Sarthi Kalathiya <sarthikalathiya@gmail.com>
2024-07-30 17:35:17 +0530

    test of basic function

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <string>
 
-
 using namespace std;
 
 class StringCalculator
@@ -31,5 +30,18 @@ public:
 int main()
 {
     StringCalculator calculator;
+
+    string input1 = "";
+    cout << "Input: \"" << input1 << "\", Output: " << calculator.add(input1) << endl;
+
+    string input2 = "1";
+    cout << "Input: \"" << input2 << "\", Output: " << calculator.add(input2) << endl;
+
+    string input3 = "1,5";
+    cout << "Input: \"" << input3 << "\", Output: " << calculator.add(input3) << endl;
+
+    string input4 = "1,2,3,4,5";
+    cout << "Input: \"" << input4 << "\", Output: " << calculator.add(input4) << endl;
+
     return 0;
 }
