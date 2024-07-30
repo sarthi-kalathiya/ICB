c3af0df2b116905392e47b103f9e87980fa4d9cf
Sarthi Kalathiya <sarthikalathiya@gmail.com>
2024-07-30 17:30:37 +0530

    created class with basic function

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,8 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+class StringCalculator
+{
+public:
+    int add(const string &numbers)
+    {
+        if (numbers.empty())
+        {
+            return 0;
+        }
+
+        stringstream ss(numbers);
+        string item;
+        int sum = 0;
+
+        while (getline(ss, item, ','))
+        {
+            sum += stoi(item);
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    StringCalculator calculator;
     return 0;
 }
